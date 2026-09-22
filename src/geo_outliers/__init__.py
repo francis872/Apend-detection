bd9fe6b4edc36e0615caf8e9e0c12b980c47d6a1
francis872 <franciscogloopez@gmail.com>
2026-09-22 16:30:54 -0500

    Export spatial and temporal validators

--- src/geo_outliers/__init__.py
+++ src/geo_outliers/__init__.py
@@ -1,6 +1,8 @@
 from .io import find_shapefiles, append_shapefiles, load_and_append, basic_clean
 from .detector import detect_outliers, remove_noise
 from .probability import fit_distributions, bhattacharyya_distance, fisher_rao_distance_discrete
+from .spatial import spatial_validation
+from .temporal import temporal_validation
 from .quadrature import (
     gaussian_quadrature, distribution_interval_probability, quadrature_cdf,
     quadrature_tail_score, confidence_interval_areas,
@@ -11,5 +13,5 @@ __all__ = [
     'detect_outliers','remove_noise','fit_distributions',
     'bhattacharyya_distance','fisher_rao_distance_discrete',
     'gaussian_quadrature','distribution_interval_probability','quadrature_cdf',
-    'quadrature_tail_score','confidence_interval_areas',
+    'quadrature_tail_score','confidence_interval_areas','spatial_validation','temporal_validation',
 ]
